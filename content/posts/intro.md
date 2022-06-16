47a1036fd24b7e41145a07598692274b482b4f59
Cloud Huxley <cloudbhuxley@gmail.com>
2022-06-16 14:38:04 -0700

    Initiates Framework.

---
+++ content/posts/intro.md
@@ -0,0 +1,6 @@
+---
+title: "Intro"
+date: 2022-06-14T17:58:31-07:00
+draft: true
+---
+
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './lib/index.js',
+  devtool: 'inline-source-map',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'themes/cloudstrap/static/js'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader',{loader: 'sass-loader',options:{implementation: require('sass'),sassOptions: {fiber: false,},},}, ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+};
